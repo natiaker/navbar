b465216a11e860f2df8acc292eef10b2c475701b
natiaker <n.kerdzaia@gmail.com>
2022-12-24 22:19:53 +0400

    Configure React app for deployment to GitHub Pages

---
+++ src/Navbar.js
@@ -0,0 +1,55 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+
+const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null);
+    const linksRef = useRef(null);
+
+    useEffect(() => {
+        const linksHeight = linksRef.current.getBoundingClientRect().height;
+        console.log(linksHeight);
+        if(showLinks) {
+            linksContainerRef.current.style.height = `${linksHeight}px`;
+        } else {
+            linksContainerRef.current.style.height = '0px'; 
+        }
+    }, [showLinks])
+    return (
+        <nav>
+            <div className="nav-center">
+                <div className="nav-header">
+                    <img src="https://react-projects-11-navbar.netlify.app/static/media/logo.2bb7da65.svg" alt="logo" />
+                    <button className='nav-toggle' onClick={()=>setShowLinks(!showLinks)}>
+                        <FaBars />
+                    </button>
+                </div>
+                <div className="links-container show-container" ref={linksContainerRef}>
+                    <ul className="links" ref={linksRef}>
+                        {links.map((link) => {
+                            const {id, url, text} = link;
+                            return (
+                                <li key={id}>
+                                    <a href={url}>{text}</a>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <ul className='social-icons'>
+                    {social.map((socialIcon) => {
+                        const {id, url, icon} = socialIcon;
+                        return (
+                            <li key={id}>
+                                <a href={url}>{icon}</a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
